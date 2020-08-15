968165f0520105f643f15cd7670871214ead7201
Eduardo Duarte <eduduarte.dev@gmail.com>
2020-08-15 19:13:33 -0300

    Atualizando código de renderização de texto assincrono

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const path = require('path');
 const basePath = 'assets/';
 //const res = document.getElementById('content');
 
-function cb (err, data) {
+function cb (err, data, index, max) {
     if(err) throw err
     console.log(data)
+    return start(index + 1, max)
 }
 console.log('Begin')
 
@@ -17,13 +18,28 @@ console.log('Begin')
 //     fs.readFile(path.resolve(basePath, file), { encoding: 'utf-8' }, cb)
 // }
 
-fs.readdir(path.resolve(basePath), (err, files) => {
-    if(err) throw err
-    files
-        .filter((file) => /s[1-3].txt/gi.test(file))// filter the files 
-        .forEach((sentenceFile) => {// for each sentence, reads and prints (async)
-            fs.readFile(path.resolve(basePath, sentenceFile), { encoding: 'UTF-8' }, cb)//will print unordered
-        })
-})
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// fs.readdir(path.resolve(basePath), (err, files) => {
+//     if(err) throw err
+//     files
+//         .filter((file) => /s[1-3].txt/gi.test(file))// filter the files 
+//         .forEach((sentenceFile) => {// for each sentence, reads and prints (async)
+//             fs.readFile(path.resolve(basePath, sentenceFile), { encoding: 'UTF-8' }, cb)//will print unordered
+//         })
+// })
+
+start(1, 4)
 
 console.log('End')
+
+function start(index, max) {
+
+    if(index > max) return
+    fs.readFile(
+        path.resolve(basePath, `s${index}.txt`),
+        { encoding: 'utf-8' },
+        (err, data) => cb(err, data, index, max)
+    )
+
+}
